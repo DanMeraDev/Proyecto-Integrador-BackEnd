8b161067a705b662499ce254a10beaf41772ea24
DanMeraDev <dm375211@gmail.com>
2024-05-31 20:47:18 -0500

    get_odontologos

--- src/main/resources/static/js/get_odontologos.js
+++ src/main/resources/static/js/get_odontologos.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function () {
 
       //con fetch invocamos a la API de peliculas con el método GET
       //nos devolverá un JSON con una colección de peliculas
-      const url = '/peliculas';
+      const url = '/odontologos';
       const settings = {
         method: 'GET'
       }
@@ -12,20 +12,21 @@ window.addEventListener('load', function () {
       .then(response => response.json())
       .then(data => {
       //recorremos la colección de peliculas del JSON
-         for(pelicula of data){
+         for(odontologo of data){
+            console.log(odontologo.id)
             //por cada pelicula armaremos una fila de la tabla
             //cada fila tendrá un id que luego nos permitirá borrar la fila si eliminamos la pelicula
-            var table = document.getElementById("peliculaTable");
-            var peliculaRow =table.insertRow();
-            let tr_id = 'tr_' + pelicula.id;
-            peliculaRow.id = tr_id;
+            var table = document.getElementById("odontologoTable");
+            var odontologoRow =table.insertRow();
+            let tr_id = odontologo.id;
+            odontologo.id = tr_id;
 
             //por cada pelicula creamos un boton delete que agregaremos en cada fila para poder eliminar la misma
             //dicho boton invocara a la funcion de java script deleteByKey que se encargará
             //de llamar a la API para eliminar una pelicula
             let deleteButton = '<button' +
-                                      ' id=' + '\"' + 'btn_delete_' + pelicula.id + '\"' +
-                                      ' type="button" onclick="deleteBy('+pelicula.id+')" class="btn btn-danger btn_delete">' +
+                                      ' id=' + '\"' + 'btn_delete_' + odontologo.id + '\"' +
+                                      ' type="button" onclick="deleteBy('+odontologo.id+')" class="btn btn-danger btn_delete">' +
                                       '&times' +
                                       '</button>';
 
@@ -33,18 +34,19 @@ window.addEventListener('load', function () {
             //a la función de java script findBy que se encargará de buscar la pelicula que queremos
             //modificar y mostrar los datos de la misma en un formulario.
             let updateButton = '<button' +
-                                      ' id=' + '\"' + 'btn_id_' + pelicula.id + '\"' +
-                                      ' type="button" onclick="findBy('+pelicula.id+')" class="btn btn-info btn_id">' +
-                                      pelicula.id +
+                                      ' id=' + '\"' + 'btn_id_' + odontologo.id + '\"' +
+                                      ' type="button" onclick="findBy('+odontologo.id+')" class="btn btn-info btn_id">' +
+                                      odontologo.id +
                                       '</button>';
 
             //armamos cada columna de la fila
             //como primer columna pondremos el boton modificar
             //luego los datos de la pelicula
             //como ultima columna el boton eliminar
-            peliculaRow.innerHTML = '<td>' + updateButton + '</td>' +
-                    '<td class=\"td_titulo\">' + pelicula.titulo.toUpperCase() + '</td>' +
-                    '<td class=\"td_categoria\">' + pelicula.categoria.toUpperCase() + '</td>' +
+            odontologoRow.innerHTML = '<td>' + updateButton + '</td>' +
+                    '<td class=\"td_matricula\">' + odontologo.matricula.toUpperCase() + '</td>' +
+                    '<td class=\"td_nombre\">' + odontologo.nombre.toUpperCase() + '</td>' +
+                    '<td class=\"td_apellido\">' + odontologo.apellido.toUpperCase() + '</td>' +
                     '<td>' + deleteButton + '</td>';
 
         };
@@ -54,7 +56,7 @@ window.addEventListener('load', function () {
 
     (function(){
       let pathname = window.location.pathname;
-      if (pathname == "/peliculaList.html") {
+      if (pathname == "/get_odontologo.html") {
           document.querySelector(".nav .nav-item a:last").addClass("active");
       }
     })
