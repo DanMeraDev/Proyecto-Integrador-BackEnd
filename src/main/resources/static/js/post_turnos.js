3a50796d488120c039944c3f331cdf50556403a8
DanaheMurilloRuiz <danahe.murillo.ruiz@gmail.com>
2024-06-23 23:15:36 -0500

    mensajes de agregado

--- src/main/resources/static/js/post_turnos.js
+++ src/main/resources/static/js/post_turnos.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function () {
 
     //Ante un submit del formulario se ejecutará la siguiente funcion
     formulario.addEventListener('submit', function (event) {
-
+        event.preventDefault();
        //creamos un JSON que tendrá los datos de la nueva película
         const formData = {
             paciente: {
@@ -33,26 +33,41 @@ window.addEventListener('load', function () {
             .then(data => {
                  //Si no hay ningun error se muestra un mensaje diciendo que la pelicula
                  //se agrego bien
-                 let successAlert = '<div class="alert alert-success alert-dismissible">' +
-                     '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
-                     '<strong></strong> turno agregada </div>'
+                 let successAlert = '<div class="alert alert-success">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
+                    '<span class="glyphicon glyphicon-ok"></span> <strong>Turno creado</strong>' +
+                    '<hr class="message-inner-separator">' +
+                    '<p>Se creo turno correctamente</p>' +
+                    '</div>';
 
                  document.querySelector('#response').innerHTML = successAlert;
                  document.querySelector('#response').style.display = "block";
                  resetUploadForm();
+                 setTimeout(() => {
+                    document.querySelector('#response').style.display = "none";
+                }, 2000); 
 
             })
             .catch(error => {
                     //Si hay algun error se muestra un mensaje diciendo que la pelicula
                     //no se pudo guardar y se intente nuevamente
-                    let errorAlert = '<div class="alert alert-danger alert-dismissible">' +
-                                     '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
-                                     '<strong> Error intente nuevamente</strong> </div>'
+                    let errorAlert = '<div>' +
+                        '<div class="alert alert-warning alert-dismissible">' +
+                            '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                            '<span class="glyphicon glyphicon-record"></span> <strong>Turno no creado</strong>' +
+                            '<hr class="message-inner-separator">' +
+                            '<p>Ocurrió un error al crear turno</p>' +
+                        '</div>' +
+                    '</div>';
 
                       document.querySelector('#response').innerHTML = errorAlert;
                       document.querySelector('#response').style.display = "block";
                      //se dejan todos los campos vacíos por si se quiere ingresar otra pelicula
-                     resetUploadForm();})
+                    resetUploadForm();
+                    setTimeout(() => {
+                        document.querySelector('#response').style.display = "none";
+                    }, 2000); 
+                })
     });
 
 
